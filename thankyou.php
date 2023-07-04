7dd6614dbe8fdc15227a43a671bd3c99a9c301d8
KanaeKobayashi <fuwaran@gmail.com>
2023-07-05 00:10:55 +0900

    リダイレクト処理の追加

---
+++ thankyou.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/write.css">
+</head>
+<body>
+<div class="message">
+    <p class="comment">オススメの本が登録されました。<br> ありがとうございました！<br></p>
+    <a href="index.php" class="back-button">戻る<br></a>
+    <a href='result.php' class="back-button">オススメ一覧へ</a>
+
+  </div>
+</body>
+</html>
--- write.php
+++ write.php
@@ -22,22 +22,10 @@ $sql = "INSERT INTO gs_bm_table (name, email, bookTitle, author, rating, comment
   $stmt->bindParam(':comment', $comment, PDO::PARAM_STR);
   $stmt->execute();
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./css/write.css">
-</head>
-<body>
-<div class="message">
-    <p class="comment">オススメの本が登録されました。<br> ありがとうございました！<br></p>
-    <a href="index.php" class="back-button">戻る<br></a>
-    <a href='result.php' class="back-button">オススメ一覧へ</a>
+   // POST処理の最後にリダイレクト処理
+  header('Location: thankyou.php');
+exit();
 
-  </div>
-</body>
-</html>
+?>
+
 
